14a4a61f6e97d69c2a45ca9a2f9206b39abee8bd
apd <andrewcodes404@gmail.com>
2018-12-12 14:55:25 +0000

    prima

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { formValue: "", results: null };
+  }
+
+  handleChange = (event) => {
+    this.setState({ formValue: event.target.value });
+  }
+
+  fetchData = (event) => {
+    event.preventDefault();
+    const Url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
+    const Api = 'a69e1cdbb16b4f23841c8f01be77f31a'
+    const formValue = this.state.formValue
+    const theQuery = Url + '?&api-key=' + Api + '&q=' + formValue
+    fetch(theQuery)
+      .then(
+        (res) => res.json()
+      )
+      .then((res) => {
+        console.log("res.response.docs = ", res.response.docs)
+        this.setState({
+          formValue: "",
+          results: res.response.docs
+        })
+      })
+      .catch(function (error) {
+        console.log('Looks like there was a problem: \n', error);
+      });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div className="logo">
+       <img src="https://www.nytco.com/wp-content/themes/nytco/images/nytco/NYT-wordmark.png" alt="new york times logo"/>
+       </div> 
+
+        <form>
+          <input type="text" value={this.state.formValue} onChange={this.handleChange} />
+          <button onClick={this.fetchData}>search</button>
+        </form>
+
+        {this.state.results && <div>
+          <h2>the results</h2>
+          {this.state.results.map((el, index) => (
+            <div key={index}>
+              <p>{el.headline.main} - <a target="_blank" rel="noopener noreferrer" href={el.web_url}>link</a></p>
+            </div>
+          )
+          )}
+
+        </div>
+        }
+
       </div>
     );
   }
